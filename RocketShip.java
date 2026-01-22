0756a6115b70c01dfc2d52b0665df06a91af18ab
Inkera O <iovado3567@students.cptc.edu>
2026-01-21 23:15:58 -0800

    implement rocket cone

--- RocketShip.java
+++ RocketShip.java
@@ -2,63 +2,86 @@
 
 public class RocketShip {
     public static final int ROCKET_SIZE = 3;
-    
-    public static void main( String[] args ) {
+
+    public static void main(String[] args) {
         printNoseCone();
         printBody();
         printNozzle();
     }
 
     /**
-     Print the nozzle at the bottom of the rocket.
-     This reuses the pattern of the nose cone.
+     * Print the nozzle at the bottom of the rocket.
+     * This reuses the pattern of the nose cone.
      */
     public static void printNozzle() {
         printNoseCone();
     }
-    
+
     /**
-     Print the nose cone. There is a pattern of **
-     down the middle of the cone with a widening pattern
-     of //...\\ on either side.
+     * Print the nose cone. There is a pattern of **
+     * down the middle of the cone with a widening pattern
+     * of //...\\ on either side.
      */
     public static void printNoseCone() {
-        // your code goes here.
+        
+        for (int i = 1; i <= ROCKET_SIZE; i ++) {
+            
+            
+            for (int h = 1; h <= ROCKET_SIZE - i; h ++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+            System.out.print("/");
+            }
+            
+            for (int k = 1; k <= i; k += 3) {
+                System.err.print("**");
+            }
+
+            for (int l = 1; l <= i; l++) {
+                System.out.print("\\");
+            }
+            System.out.println();
+        }
+        
         System.out.println("A nose cone should be here.");
+        
     }
-    
+
     /**
-      Print body of rocket. This is a two-stage pattern
-      with a diamond pattern in the top half and an
-      hour glass on the bottom.
+     * Print body of rocket. This is a two-stage pattern
+     * with a diamond pattern in the top half and an
+     * hour glass on the bottom.
      */
     public static void printBody() {
         printEdge();
         printDiamondTop();
-        printDiamondBottom();        
+        printDiamondBottom();
         printEdge();
-        printDiamondBottom();        
-        printDiamondTop();       
-        printEdge();        
+        printDiamondBottom();
+        printDiamondTop();
+        printEdge();
     }
-    
+
     /**
-     Print the top half of two diamonds between vertical lines.
+     * Print the top half of two diamonds between vertical lines.
      */
     public static void printDiamondTop() {
         // your code goes here.
         System.out.println("A diamond top should be here.");
     }
-    
+
     /**
-     Print the bottom half of two diamonds between vertical lines.
+     * Print the bottom half of two diamonds between vertical lines.
      */
     public static void printDiamondBottom() {
         // your code goes here.
         System.out.println("A diamond bottom should be here.");
     }
+
     /**
-      Print +=*=*=*...=+ to fit the width of the rocket.
+     * Print +=*=*=*...=+ to fit the width of the rocket.
      */
     public static void printEdge() {
         // your code goes here.
